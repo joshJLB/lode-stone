0c9e6e3715eefba9c65aa7db3417798b05621aef
unknown <josh@jlbworks.com>
2018-09-07 11:28:32 -0500

    Added section one and mobilized.

--- themes/JLBTheme/front-page.php
+++ themes/JLBTheme/front-page.php
@@ -8,12 +8,20 @@ get_header(); ?>
 <main id="home">
 
   <?php get_template_part('components/home-page/hero'); ?>
-  <!-- Or, nah -->
-  <section class="hero">
-
-  </section>
 
   <!-- Continue Sections -->
+
+  <section class="one">
+    <div class="one-container">
+      <div class="one-content">
+        <img src="<?=get_field('one_icon'); ?>" alt="">
+        <h2><?=get_field('one_title'); ?></h2>
+        <p><?=get_field('one_content'); ?></p>
+        <a href="<?=get_field('one_link_url'); ?>"><?=get_field('one_link_text'); ?> >></a>
+      </div>
+      <div class="one-image" style="background-image:url(<?=get_field('one_image'); ?>)"></div>
+    </div>
+  </section>
 
 </main>
 
