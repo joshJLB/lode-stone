cd8561a7b866d6cf857cc5872accd044a13c58f1
unknown <josh@jlbworks.com>
2018-09-12 11:28:43 -0500

    Added contact page

--- themes/JLBTheme/contact.php
+++ themes/JLBTheme/contact.php
@@ -10,6 +10,31 @@ get_header(); ?>
 
   <div class="contact-container">
     <!-- Site Origin or Custom, you decide! -->
+    <h2><?=get_field('contact_title', 'option'); ?></h2>
+    <h4><?=get_field('contact_sub_title', 'option'); ?></h4>
+    <div class="contact-icon-containers">
+      <div class="contact-icon">
+        <a href="tel:<?=get_field('phone', 'option'); ?>">
+          <i class="fas fa-mobile-alt"></i>
+          <p>Call us</p>
+        </a>
+      </div>
+      <div class="contact-icon">
+        <a href="tel:<?=get_field('phone', 'option'); ?>">
+          <i class="fas fa-map-marker-alt"></i>
+          <p>Visit us</p>
+        </a>
+      </div>
+      <div class="contact-icon">
+        <a href="mailto:<?=get_field('email', 'option'); ?>">
+          <i class="fas fa-envelope"></i>
+          <p>Email us</p>
+        </a>
+      </div>
+    </div>
+    <div class="contact-form">
+      <?php gravity_form(1); ?>
+    </div>
   </div>
 
 </main>
